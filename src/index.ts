77a38591347e16f22643b7f8051fcf2d41f77e74
zhesha <savzhe@gmail.com>
2020-12-13 14:08:23 +0200

    added isolatedPlay and isolatedLoop

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import {DeltaTime, log, LogLevel, Asset, SpriteMap} from "core";
+import {DeltaTime, log, LogLevel, Asset, SpriteMap, Sprite} from "core";
 
 interface AnimationDataParam {
   [index: string]: Array<number>
+}
+
+interface AnimationResult {
+  timer: number,
+  sprite?: Sprite,
 }
 
 export class AnimationData {
@@ -32,17 +37,51 @@ export class Animation {
 
   play (name: string, delta: DeltaTime){
     if (this.name !== name) {
+      this.timer = 0;
       return this.start(name);
     } else {
-      return this.continue(name, delta, (_, length) => length - 1);
+      this.timer += delta;
+      return this.continue(name, delta, this.timer, (_, length) => length - 1);
     }
   }
 
   loop (name: string, delta: DeltaTime){
     if (this.name !== name) {
+      this.timer = 0;
       return this.start(name);
     } else {
-      return this.continue(name, delta, (index, length) => index % length);
+      this.timer += delta;
+      return this.continue(name, delta, this.timer, (index, length) => index % length);
+    }
+  }
+
+  isolatedPlay (name: string, delta: DeltaTime, timer: number): AnimationResult {
+    if (this.name !== name) {
+      return {
+        timer: 0,
+        sprite: this.start(name)
+      }
+    } else {
+      timer += delta;
+      return {
+        timer,
+        sprite: this.continue(name, delta, timer, (_, length) => length - 1)
+      }
+    }
+  }
+
+  isolatedLoop (name: string, delta: DeltaTime, timer: number): AnimationResult {
+    if (this.name !== name) {
+      return {
+        timer: 0,
+        sprite: this.start(name)
+      }
+    } else {
+      timer += delta;
+      return {
+        timer,
+        sprite: this.continue(name, delta, timer, (index, length) => index % length)
+      }
     }
   }
 
@@ -52,7 +91,6 @@ export class Animation {
 
   private start(name: string) {
     this.name = name;
-    this.timer = 0;
 
     const animation = this.animationData.getAnimation(name);
     if (!animation) {
@@ -62,13 +100,12 @@ export class Animation {
     return this.animationData.getSprite(spriteIndex);
   }
 
-  private continue(name: string, delta: DeltaTime, resolveIndexOverflow: (index: number, length: number) => number) {
-    this.timer += delta;
+  private continue(name: string, delta: DeltaTime, timer: number, resolveIndexOverflow: (index: number, length: number) => number) {
     const animation = this.animationData.getAnimation(name);
     if (!animation) {
       return;
     }
-    let frameIndex = this.calculateFrameIndex(delta);
+    let frameIndex = this.calculateFrameIndex(delta, timer);
     if (frameIndex >= animation.length) {
       frameIndex = resolveIndexOverflow(frameIndex, animation.length);
     }
@@ -76,8 +113,8 @@ export class Animation {
     return this.animationData.getSprite(spriteIndex);
   }
 
-  calculateFrameIndex (_: DeltaTime) {
+  calculateFrameIndex (_: DeltaTime, timer: number) {
     const rate = 1000 / this.speed;
-    return Math.floor( this.timer / rate);
+    return Math.floor( timer / rate);
   }
 }
